48eff4398db0ec4bed6ad56fbe78adcfafb699ec
Gary Cornelius <garycorn90@gmail.com>
2016-08-08 14:17:01 +0200

    Added FALSE test case.

--- test/ros_mac_authentication_test_1.cpp
+++ test/ros_mac_authentication_test_1.cpp
@@ -26,6 +26,22 @@ TEST(RosHashAuthentication, validAuthentication)
 
   EXPECT_TRUE(client.call(srv));
   EXPECT_TRUE(srv.response.authenticated);
+
+  string msg1 = "bc8c629cd7477fd580b8f9e8da49e5aad364b769192.168.1.101192.168.1.1111337";
+  string sign1 = "ff436753ef5b30b77951707b893c65343129c51d79d0a67bb746830ee7d6412bfd730c46b5b9be3a4651f542a163c7b7a38b711bea6e16f3cc4bc478c382f301";
+  string addr1 = "a28779d29c49fd57d0fc4130e5ebec07c2c79ef5";
+
+    // make the request
+  rosauth::Authentication srv1;
+  srv1.request.msg = msg1;
+  srv1.request.sign = sign1;
+  srv1.request.addr = addr1;
+
+
+  EXPECT_TRUE(client.call(srv1));
+  EXPECT_FALSE(srv1.response.authenticated);
+
+
 }
 
 // Run all the tests that were declared with TEST()
